40d532cdc1ef3e77c159cad2ccee0baced83d6bc
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-05 16:25:18 -0400

    Add no more plot armor mutator

---
+++ mutators.lua
@@ -0,0 +1,19 @@
+local MUTATORS =
+{
+    no_more_plot_armour =
+    {
+        name = "No More Plot Armour!",
+        desc = "Plot armour for characters simply do not exist.\n\nFssh can hate you, Hebbel can love you, Moreef can die, game can softlock. Choose this mutator at your own risk.",
+
+        event_handlers =
+        {
+            calculate_plot_armour = function( self, agent, params )
+                params.override_has_plot_armour = false
+            end
+        },
+    },
+}
+
+for id, graft in pairs( MUTATORS ) do
+    Content.AddMutatorGraft( id, graft )
+end
--- patches/remove_plot_armor.lua
+++ patches/remove_plot_armor.lua
@@ -3,8 +3,8 @@ local old_plot_armour_fn = AgentUtil.HasPlotArmour
 function AgentUtil.HasPlotArmour(agent, ...)
     local plot_armour_data = {}
     TheGame:BroadcastEvent( "calculate_plot_armour", agent, plot_armour_data )
-    if plot_armour_data.override_plot_armour ~= nil then
-        return plot_armour_data.override_plot_armour
+    if plot_armour_data.override_has_plot_armour ~= nil then
+        return plot_armour_data.override_has_plot_armour
     end
     return old_plot_armour_fn(agent, ...)
 end
