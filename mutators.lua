6b291f9cad17698079eca2be779741cf36cc0464
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-05 16:33:58 -0400

    Update mutators.lua

--- mutators.lua
+++ mutators.lua
@@ -1,6 +1,6 @@
 local MUTATORS =
 {
-    no_more_plot_armour =
+    GED_no_more_plot_armour =
     {
         name = "No More Plot Armour!",
         desc = "Plot armour for characters simply do not exist.\n\nFssh can hate you, Hebbel can love you, Moreef can die, game can softlock. Choose this mutator at your own risk.",
