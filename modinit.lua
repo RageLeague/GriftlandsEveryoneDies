5f89f78e1e76b55c0a000fd2142feda922576ca0
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-06 15:07:46 -0400

    Add new settings

--- modinit.lua
+++ modinit.lua
@@ -11,6 +11,43 @@ local function OnLoad( mod )
     require "EveryoneDies:mutators"
 end
 
+local MOD_OPTIONS =
+{
+    {
+        title = "Allow Provoke Anywhere",
+        spinner = true,
+        key = "allow_provoke_anywhere",
+        default_value = false,
+        values =
+        {
+            { name="Disable (Default)", desc="Vanilla behaviour. You cannot provoke a character in a location labelled \"HQ\".", data = false },
+            { name="Enable", desc="Location restriction for provokation no longer applies, and you may provoke characters at HQs.", data = true },
+        }
+    },
+    {
+        title = "Allow Provoke Anyone",
+        spinner = true,
+        key = "allow_provoke_anyone",
+        default_value = false,
+        values =
+        {
+            { name="Disable (Default)", desc="Vanilla behaviour. Plot armour characters, people in your party, and people involved in quests can't be provoked.", data = false },
+            { name="Enable", desc="Everyone can be provoked given the right condition.", data = true },
+        }
+    },
+    {
+        title = "Allow Any Relation Provoking",
+        spinner = true,
+        key = "allow_any_relation_provoking",
+        default_value = false,
+        values =
+        {
+            { name="Disable (Default)", desc="Vanilla behaviour. You can only provoke hated characters.", data = false },
+            { name="Enable", desc="You can provoke anyone, from disliked to even loved (if you want for some reason).", data = true },
+        }
+    },
+}
+
 return {
     version = "0.0.1",
     alias = "EveryoneDies",
