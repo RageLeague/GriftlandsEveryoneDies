24a6bdda24cb85c2a4d2c8c6bd4a8c83bb262313
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-06 09:17:30 -0400

    Update mutators.lua

--- mutators.lua
+++ mutators.lua
@@ -16,6 +16,43 @@ local MUTATORS =
     {
         name = "No Innate Plot Armour",
         desc = "Certain characters, such as Fssh and night merchants, no longer have plot armour by default.",
+
+        event_handlers =
+        {
+            calculate_agent_has_tag = function( self, params, agent, tag )
+                if tag == "plot_armour" then
+                    params.override_has_plot_armour = false
+                end
+            end
+        },
+    },
+    GED_everyone_actually_gets_thirsty =
+    {
+        name = "Everyone Actually Gets Thirsty",
+        desc = "People who normally cannot visit now can (other conditions apply).",
+
+        event_handlers =
+        {
+            calculate_agent_has_tag = function( self, params, agent, tag )
+                if tag == "no_patron" then
+                    params.override_has_plot_armour = false
+                end
+            end
+        },
+    },
+    GED_no_man_is_an_island =
+    {
+        name = "No Man Is An Island",
+        desc = "People who normally don't have friends now can have friends to be sad about when they eventually die (other conditions apply).",
+
+        event_handlers =
+        {
+            calculate_agent_has_tag = function( self, params, agent, tag )
+                if tag == "no_friends" then
+                    params.override_has_plot_armour = false
+                end
+            end
+        },
     },
 }
 
